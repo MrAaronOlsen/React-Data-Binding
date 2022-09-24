1528c3a99f9acef39f2f6e4ad50337953298f4a7
Aaron Olsen <aaron.olsen@nextworld.net>
2022-09-24 13:56:21 -0600

    Initialize

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './root.scss'
+import styles from './styles.mod.scss'
+
+export default function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.welcome}>Welcome to App</div>
+    </div>
+  )
+}
